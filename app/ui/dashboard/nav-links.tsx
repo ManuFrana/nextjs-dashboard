a15229d4f5e525b38fc538cc1dbf52c8f6b33886
ManuFrana <manuel.frana@hotmail.com>
2025-03-04 13:19:46 -0300

    fix based on deploy error message

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,6 +4,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
